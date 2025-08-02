cbccfdad2eb06d811a88cc81f920709a4a45fab3
JaydipJadhav25 <jaydipdjadhav@gmail.com>
2025-08-03 01:54:39 +0530

    setup tailwind and shadcn and also added dark mode effect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
-
-import './App.css'
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from "./components/mode-toggle"
 
 function App() {
-
-
   return (
-    <>
-   <h1>Smart Mess</h1>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+       <div className="p-40">
+        <h1 className="bg-gradient-to-r from-background via-yellow-200 to-orange-600">smart mess</h1>
+       </div>
+      <ModeToggle/>
+    </div>
   )
 }
 
